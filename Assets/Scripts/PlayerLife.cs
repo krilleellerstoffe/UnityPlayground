1d5ba55943714afe69285372e42c44e98ae88417
Christopher O'Driscoll <77005138+krilleellerstoffe@users.noreply.github.com>
2023-04-08 13:09:23 +0200

    traps and fruits
    strawberries and spikes

---
+++ Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody2D body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //If hitting a trap
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        //trigger death animation
+        animator.SetTrigger("death");
+        //disable movement
+        body.bodyType = RigidbodyType2D.Static;
+    }
+    //restart level
+    private void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,13 +6,14 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D body;
     private BoxCollider2D boxCollider;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
     private float dirX;
     [SerializeField] private float jumpForce = 14f;
     [SerializeField] private float moveSpeed = 6f;
-    [SerializeField] private LayerMask jumpableGround;
-    private SpriteRenderer spriteRenderer;
-    private Animator animator;
-
+    [SerializeField] private LayerMask ground;
+    
+    //enum for storing different movement states
     private enum MoveState
     {
         idling,
@@ -34,45 +35,50 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         dirX = Input.GetAxisRaw("Horizontal");
         body.velocity = new Vector2(dirX * moveSpeed, body.velocity.y);
-        if (Input.GetButtonDown("Jump") && IsOnGround())
+        if (Input.GetButtonDown("Jump") && IsOnGround()) //only jump if on ground
         {
             body.velocity = new Vector2(body.velocity.x, jumpForce);
         }
         UpdateAnimationState();
     }
+    //Updates the sprite's animation based on movement
     private void UpdateAnimationState()
     {
         MoveState moveState;
-
-        if(dirX > 0f)
+       
+        if(dirX > 0f) //if moving to right
         {
             spriteRenderer.flipX = false;
             moveState = MoveState.running;
         }
-        else if(dirX < 0f)
+        else if(dirX < 0f) //if moving to left
         {
             spriteRenderer.flipX = true;
             moveState = MoveState.running;
         }
-        else
+        else //if still
         {
             moveState = MoveState.idling;
         }
-        if(body.velocity.y > 0.1f) 
+        
+        if(body.velocity.y > 0.1f) //if jumping
         {
             moveState = MoveState.jumping;
         }
-        else if (body.velocity.y < -0.1f)
+        else if (body.velocity.y < -0.1f) //if falling
         {
             moveState = MoveState.falling;
         }
+        //set animation based on moveState
         animator.SetInteger("moveState", (int)moveState);
     }
-
+    //Creates a boxcollider slightly lower than the player sprite's
+    //returns true if it is touching the ground
     private bool IsOnGround()
     {
-        return Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.down, 0.1f, jumpableGround);
+        return Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.down, 0.1f, ground);
     }
 }
